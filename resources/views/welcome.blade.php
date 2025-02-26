c46b648ae62e788d95aabe9d9b975f109ac98b61
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-02-26 12:30:09 +0300

    Update title in welcome view and fix Vite entry point syntax

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wardrobe App</title>
-    @vite(entrypoints: ['resources/js/app.js'])
+    <title>My Wardrobe App</title>
+    @vite([resources/js/app.js])
 </head>
 <body>
     <div id="app"></div>
