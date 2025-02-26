13e6938d4ae7900726d0d19e35a3d21c2429991a
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-02-26 14:26:07 +0300

    Update import for Laravel Vite plugin in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import laravel from 'vite-plugin-laravel';
 
 export default defineConfig({
     plugins: [
