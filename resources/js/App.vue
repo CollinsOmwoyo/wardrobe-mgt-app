edf2bc1266d232928c16ae1d97f2b206654f14c0
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-02-26 17:31:32 +0300

    Update key binding in router-view for improved component rendering

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,6 @@
 <template>
     <router-view v-slot="{ Component, route }">
-        <div :key="route.fullPath">
+        <div :key="route.name">
             <component :is="Component" />
         </div>
     </router-view>
